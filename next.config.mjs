658ecff24b8f0efd39e5565fd3429735c59d4f6a
Saikot hasan <49225310+saikothasan@users.noreply.github.com>
2024-12-26 21:49:08 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Set the output to static HTML export
+  output: 'export',
+
+  // Optionally specify other configurations, such as basePath, trailingSlash, etc.
+  // basePath: '/my-app',
+  // trailingSlash: true,
+};
 
 export default nextConfig;
